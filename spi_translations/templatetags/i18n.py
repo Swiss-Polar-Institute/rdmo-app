eff11974c62535fb6dfa2b36759660c52ee81ffe
Carles Pina i Estany <carles@pina.cat>
2021-05-30 17:56:54 +0200

    Fix error because the import is not keeping the decorators

--- spi_translations/templatetags/i18n.py
+++ spi_translations/templatetags/i18n.py
@@ -43,3 +43,63 @@ def spi_do_block_translate(parser, token):
                                  translated.plural, translated.countervar,
                                  translated.counter, translated.message_context,
                                  translated.trimmed, translated.asvar)
+
+
+@register.tag("get_available_languages")
+def spi_do_get_available_languages(parser, token):
+    return do_get_available_languages(parser, token)
+
+
+@register.tag("get_language_info")
+def spi_do_get_language_info(parser, token):
+    return do_get_language_info(parser, token)
+
+
+@register.tag("get_language_info_list")
+def spi_do_get_language_info_list(parser, token):
+    return do_get_language_info_list(parser, token)
+
+
+@register.filter
+def spi_language_name(lang_code):
+    return language_name(lang_code)
+
+
+@register.filter
+def spi_language_name_translated(lang_code):
+    return language_name_translated(lang_code)
+
+
+@register.filter
+def spi_language_name_local(lang_code):
+    return language_name_local(lang_code)
+
+
+@register.filter
+def spi_language_bidi(lang_code):
+    return language_bidi(lang_code)
+
+
+@register.tag("get_current_language")
+def spi_do_get_current_language(parser, token):
+    return do_get_current_language(parser, token)
+
+
+@register.tag("get_current_language_bidi")
+def spi_do_get_current_language_bidi(parser, token):
+    return do_get_current_language_bidi(parser, token)
+
+
+@register.tag("trans")
+def spi_do_translate(parser, token):
+    return do_translate(parser, token)
+
+
+@register.tag("blocktrans")
+def spi_do_block_translate(parser, token):
+    return do_block_translate(parser, token)
+
+
+@register.tag
+def spi_language(parser, token):
+    return language(parser, token)
