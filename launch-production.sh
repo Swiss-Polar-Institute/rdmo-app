221f4bf2d3ac7aec8d039cda04562ad84d3ea2b4
Carles Pina i Estany <carles@pina.cat>
2021-06-03 12:50:40 +0000

    Launch Docker detached

--- launch-production.sh
+++ launch-production.sh
@@ -4,5 +4,5 @@ ENV_FILE="$HOME/config/rdmo_production_env_file"
 
 docker-compose -f docker-compose.yml --env-file "$ENV_FILE" down
 
-docker-compose -f docker-compose.yml --env-file "$ENV_FILE" up --build
+docker-compose -f docker-compose.yml --env-file "$ENV_FILE" up --build -d
 
